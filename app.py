34419e21905708bc843b756931f00589e3cd3f19
Pramudi03 <pabasara.2019655@iit.ac.lk>
2022-04-21 00:39:40 +0530

    User Logout added

--- app.py
+++ app.py
@@ -246,7 +246,13 @@ def login_new():
 
     return render_template('login.html', msg=msg)
 
-
+#user Logout
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 # @app.route('/loginAdmin', methods=['GET', 'POST'])
 # def login_admin():
