685b045562b3b596e9e33937be2d30a5a287fe0e
Chamod Caldera <chamod.20200747@iit.ac.lk>
2022-05-01 00:17:46 +0530

    Database

--- dbConnection.py
+++ dbConnection.py
@@ -4,10 +4,10 @@ from flaskext.mysql import MySQL
 mysqldb = MySQL()
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'SkinSafe'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'admin'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'fOHh2cA0'
+app.config['MYSQL_DATABASE_DB'] = 'skinsafe'
+app.config['MYSQL_DATABASE_HOST'] = 'mysql-77609-0.cloudclusters.net'
 
 
 
--- setUp.py
+++ setUp.py
@@ -4,15 +4,15 @@ import pymysql
 
 
 def connection():
-    mysqldb=mysql.connector.connect(host="localhost",user="root",password="")#established connection
-    # mysqldb = pymysql.connect(
-    #     host='database-1.cfjzzf8ivqho.us-east-1.rds.amazonaws.com',
-    #     port=3306,
-    #     user='admin',
-    #     password='MC12345678',
-    #     db='database-1',
-    #
-    # )
+    # mysqldb=mysql.connector.connect(host="mysql-77609-0.cloudclusters.net",user="chamma",password="12345678")#established connection
+    mysqldb = pymysql.connect(
+        host='mysql-77609-0.cloudclusters.net',
+        port=11976,
+        user='admin',
+        password='fOHh2cA0',
+        db='skinsafe',
+
+    )
     mycursor=mysqldb.cursor()#cursor() method create a cursor object
     mycursor.execute("CREATE DATABASE IF NOT EXISTS SkinSafe")  # Execute SQL Query to create a database
     # mycursor = mysqldb.cursor()  # cursor() method create a cursor object
